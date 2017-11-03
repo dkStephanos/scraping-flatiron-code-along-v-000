cc2aa27e1992cec896d659475cbd5825a0bcc817
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-03 14:45:16 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
-    def print_courses
+
+  #Methods
+  def print_courses
     self.make_courses
     Course.all.each do |course|
       if course.title
@@ -14,6 +16,10 @@ class Scraper
         puts "  Description: #{course.description}"
       end
     end
+  end
+
+  def get_page
+    
   end
   
 end
