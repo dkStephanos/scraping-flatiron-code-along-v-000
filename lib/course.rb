86ab24c401445230a823ecbceefd984b51c31ff4
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-03 14:32:27 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,19 @@
+class Course
 
+  #Variables
+  attr_accessor :titile, :schedule, :Description
+  @@all = []
 
+  #Methods
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
